1d04a7ccd82fec99b4398bf81cfd21487e9f0eba
brensw <sweeney.b@me.com>
2019-12-22 20:19:20 +0000

    Update carsDAO.py

--- carsDAO.py
+++ carsDAO.py
@@ -3,44 +3,41 @@ import dbconfig as dbc
 import MySQLdb
 
 
-class DB:
-    conn=None
 
-    def connect(self):
-        self.comm=MySQLdb.connect()
-
-    def query(sefl, sql):
-        try:
-            cursor=self.conn.cursor()   
-            cursor.execute(sql)
-        except (AttributeError, MySQLdb.OperationalError):
-            self.connect()   
-            cursor =self.conn.cursor()
-            cursor.execute(sql)
-        return cursor    
 
 
 class CarsDAO:
-    db=""
-    def __init__(self): 
+
+    def connectDB(self):
         self.db = mysql.connector.connect(
         host= dbc.mysql['host'],
         user =dbc.mysql['user'],
         password=dbc.mysql['password'],
         database=dbc.mysql['database']
         )
-    
+
+
+    db=""
+    def __init__(self): 
+        self.connectDB()
+
+    def getCursor(self):
+        if not self.db.is_connected():
+            self.connectDB()
+            
+        return self.db.cursor()        
             
     def create(self, values):
-        cursor = self.db.cursor()
+        cursor = self.getCursor()
         sql="insert into cars (make,year, price) values (%s,%s,%s)"
         cursor.execute(sql, values)
 
         self.db.commit()
+        cursor.close()   
         return cursor.lastrowid
 
     def getAll(self):
-        cursor = self.db.cursor()
+         cursor = self.getCursor()
         sql="select * from cars"
         cursor.execute(sql)
         results = cursor.fetchall()
@@ -49,32 +46,37 @@ class CarsDAO:
         for result in results:
             print(result)
             returnArray.append(self.convertToDictionary(result))
-
+        cursor.close()    
         return returnArray
 
     def findByID(self, id):
-        cursor = self.db.cursor()
+         cursor = self.getCursor()
         sql="select * from cars where id = %s"
         values = (id,)
 
         cursor.execute(sql, values)
         result = cursor.fetchone()
+        cursor.close()   
         return self.convertToDictionary(result)
 
     def update(self, values):
-        cursor = self.db.cursor()
+        cursor = self.getCursor()
         sql="update cars set make= %s,year=%s, price=%s  where id = %s"
         cursor.execute(sql, values)
         self.db.commit()
+        cursor.close()   
+
+
     def delete(self, id):
-        cursor = self.db.cursor()
+         cursor = self.getCursor()
         sql="delete from cars where id = %s"
         values = (id,)
 
         cursor.execute(sql, values)
 
         self.db.commit()
-        print("delete done")
+        cursor.close()   
+       
 
     def convertToDictionary(self, result):
         colnames=['id','make','year', "price"]
